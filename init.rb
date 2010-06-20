f60d7c588241a4ec33d61a9be8dfbd55e6a52746
Jeena Paradies <spam@jeenaparadies.net>
2010-06-20 18:36:43 +0200

    added possibility to send plists as post parameters

--- init.rb
+++ init.rb
@@ -2,3 +2,9 @@ require 'plistifier/plist_encoding'
 require 'rails_extensions'
 
 Mime::Type.register "application/plist", :plist
+
+ActionController::Base.param_parsers[Mime::Type.lookup("application/plist")] = lambda do |body|
+  str = StringIO.new(body)
+  plist = CFPropertyList::List.new({:data => str.string})
+  CFPropertyList.native_types(plist.value)
+end
